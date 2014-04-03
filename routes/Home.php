536c92c46880711860a87d33f3b467b4cc53b290
Chris Gordon <chrisgordon13@gmail.com>
2014-04-03 16:29:26 -0700

    Testing root route

--- routes/Home.php
+++ routes/Home.php
@@ -1,12 +1,13 @@
 <?
 
 $app->get('/', function() use ($app) {
-
+/*
     $links = [
         'Hotels'=>'/Hotes',
         'Places'=>'/Places'
     ];
 
     $app->response->write(json_encode($links));
-
+*/
+    $app->response->write("Hello");
 });
