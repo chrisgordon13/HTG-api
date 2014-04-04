6cd48c95cc0adfcaa775c9b57e681e6374283d7a
Chris Gordon <chrisgordon13@gmail.com>
2014-04-04 15:34:41 -0700

    Update to home route

--- routes/Home.php
+++ routes/Home.php
@@ -1,13 +1,12 @@
 <?
 
 $app->get('/', function() use ($app) {
-/*
+
     $links = [
         'Hotels'=>'/Hotes',
         'Places'=>'/Places'
     ];
 
     $app->response->write(json_encode($links));
-*/
-    $app->response->write("Hello");
+
 });
