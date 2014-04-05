30194ffa0e416ad0068af3e7aa515dcab123cbcf
Chris Gordon <chrisgordon13@gmail.com>
2014-04-04 21:09:04 -0700

    Update to home route

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $deps['orm']            = function() {
 
                             $orm = new o;
 
-                            $orm::configure('mysql:host=10.128.12.164;dbname=PropsIO');
+                            $orm::configure('mysql:host=10.128.12.164;dbname=HealthyTravelGal');
                             $orm::configure('username', 'propsio');
                             $orm::configure('password', 'kDb6c$9a');
 
