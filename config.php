f77086ab099afdbd8e282ccfc4953c94f00240c7
Chris Gordon <chrisgordon13@gmail.com>
2014-04-05 08:11:43 -0700

    Testing db connection

--- config.php
+++ config.php
@@ -14,9 +14,13 @@ $deps['orm']            = function() {
 
                             $orm = new o;
 
-                            $orm::configure('mysql:host=10.128.12.164;dbname=PropsIO');
-                            $orm::configure('username', 'htg');
-                            $orm::configure('password', 'rt304X^H#$093JlKb@]]');
+                            //$orm::configure('mysql:host=10.128.12.164;dbname=HealthyTravelGal');
+                            //$orm::configure('username', 'htg');
+                            //$orm::configure('password', 'rt304X^H#$093JlKb@]]');
+
+                            $orm::configure('mysql:host=10.128.12.164;dbname=TripNanny');
+                            $orm::configure('username', 'tripnanny');
+                            $orm::configure('password', 'kDb6c$9a');
 
                             return $orm;
                         };
